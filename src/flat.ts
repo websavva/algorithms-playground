93257250b291f00ac4db52af6b6d813b3ea4aaae
WebSavva <websavvap@gmail.com>
2025-01-02 23:52:13 +0300

    Another minor fix was made

--- src/flat.ts
+++ src/flat.ts
@@ -1,5 +1,5 @@
 export function flat(arr: any[], n: number): any {
-  if (n > 1000) throw new Error(`n is larger than max depth of ${n}`);
+  if (n > 1_000) throw new Error(`n is larger than max depth of ${n}`);
 
   const flattenedArr: any[] = [];
 
